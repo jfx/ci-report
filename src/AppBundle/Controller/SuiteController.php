380ecd9468d2d85a7fbb760c87f6e0a47f8a23a6
FX Soubirou <soubirou@yahoo.fr>
2017-07-31 07:00:56 +0200

    Header for php files - fix: #1

--- src/AppBundle/Controller/SuiteController.php
+++ src/AppBundle/Controller/SuiteController.php
@@ -1,5 +1,22 @@
 <?php
-
+/**
+ * Copyright (c) 2017 Francois-Xavier Soubirou.
+ *
+ * This file is part of ci-report.
+ *
+ * ci-report is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ci-report is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ci-report. If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Campaign;
