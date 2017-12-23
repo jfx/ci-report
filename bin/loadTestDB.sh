bc49b00e08c7987a9e05d15d292ed200f55451f1
FX Soubirou <soubirou@yahoo.fr>
2017-12-23 09:45:54 +0100

    Syntax validation improvements

--- bin/loadTestDB.sh
+++ bin/loadTestDB.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROJECT_DIR=`dirname $0`/..
 cd $PROJECT_DIR
-php bin/console doctrine:database:drop --force --no-interaction
-php bin/console doctrine:database:create --no-interaction
-php bin/console doctrine:schema:create --no-interaction
-php bin/console doctrine:fixtures:load --no-interaction
+bin/console doctrine:database:drop --force --no-interaction
+bin/console doctrine:database:create --no-interaction
+bin/console doctrine:schema:create --no-interaction --quiet
+bin/console doctrine:fixtures:load --no-interaction
