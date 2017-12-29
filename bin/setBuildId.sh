9da338d4976d028a8af861df8e86d929bdfef913
FX Soubirou <soubirou@yahoo.fr>
2017-12-29 11:10:59 +0100

    Remove branch on build_id when master - fix: #97

--- bin/setBuildId.sh
+++ bin/setBuildId.sh
@@ -7,6 +7,9 @@ if [ "$#" -ne 1 ]; then
 else
     BUILD=$1
 fi
+if [ "${BUILD:0:7}" == "master/" ]; then
+    BUILD=${BUILD:7}
+fi
 BUILD_ESCAPED="${BUILD//\//\\/}"
 
 sed -i "s/^.*app.build:.*/    app.build: ${BUILD_ESCAPED}/" $PROJECT_DIR/app/config/parameters.yml
