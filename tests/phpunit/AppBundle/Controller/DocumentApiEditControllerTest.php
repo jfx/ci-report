97e2922db3a7166e13cc57d198a15a87828bd66f
FX Soubirou <soubirou@yahoo.fr>
2018-02-16 09:10:46 +0100

    Unit tests for Document API controller

--- tests/phpunit/AppBundle/Controller/DocumentApiEditControllerTest.php
+++ tests/phpunit/AppBundle/Controller/DocumentApiEditControllerTest.php
@@ -4,56 +4,40 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class SuiteApiEditControllerTest extends AbstractKernelControllerTest
+class DocumentApiEditControllerTest extends AbstractKernelControllerTest
 {
     protected $testFilesDir = __DIR__.'/../../../files';
 
-    public function testPostJunitSuites()
+    public function testPostDocuments()
     {
         copy(
-            $this->testFilesDir.'/junit-ok2.xml',
-            $this->testFilesDir.'/junit2upload.xml'
+            $this->testFilesDir.'/zipfile-ok.zip',
+            $this->testFilesDir.'/zipfile2upload.zip'
         );
-        $junitFile = new UploadedFile(
-            $this->testFilesDir.'/junit2upload.xml',
-            'junit2upload.xml'
+        $zipFile = new UploadedFile(
+            $this->testFilesDir.'/zipfile2upload.zip',
+            'zipfile2upload.zip'
         );
 
         $client = static::createClient();
         $client->request(
             'POST',
-            '/api/projects/project-one/campaigns/1/suites/junit',
+            '/api/projects/project-one/campaigns/4/suites/2/doc/zip',
             array('warning' => 50, 'success' => 60),
-            array('junitfile' => $junitFile),
+            array('zipfile' => $zipFile),
             array('CONTENT_TYPE' => 'multipart/form-data', 'HTTP_X-CIR-TKN' => '1f4ffb19e4b9-02278af07b7d-4e370a76f001')
         );
 
         $this->assertEquals(201, $client->getResponse()->getStatusCode());
     }
 
-    public function testPutLimitsSuites()
-    {
-        $client = static::createClient();
-
-        $client->request(
-            'PUT',
-            '/api/projects/project-one/campaigns/1/suites/1/limits',
-            array(),
-            array(),
-            array('CONTENT_TYPE' => 'application/json', 'HTTP_X-CIR-TKN' => '1f4ffb19e4b9-02278af07b7d-4e370a76f001'),
-            '{"warning":50, "success":60}'
-        );
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-    }
-
-    public function testDeleteSuites()
+    public function testDeleteDocuments()
     {
         $client = static::createClient();
 
         $client->request(
             'DELETE',
-            '/api/projects/project-one/campaigns/1/suites/1',
+            '/api/projects/project-one/campaigns/4/suites/1/doc/zip',
             array(),
             array(),
             array('HTTP_X-CIR-TKN' => '1f4ffb19e4b9-02278af07b7d-4e370a76f001')
