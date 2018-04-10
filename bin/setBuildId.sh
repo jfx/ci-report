d34ea152e5da9c58e7583e88ec4df4d692f3dd2f
FX Soubirou <soubirou@yahoo.fr>
2018-04-10 09:11:33 +0200

    Update travis for Symfony 4

---
+++ bin/setBuildId.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+PROJECT_DIR=`dirname $0`/..
+cd $PROJECT_DIR
+
+if [ "$#" -ne 1 ]; then
+    BUILD=BR/CID/BID
+else
+    BUILD=$1
+fi
+if [ "${BUILD:0:7}" == "master/" ]; then
+    BUILD=${BUILD:7}
+fi
+BUILD_ESCAPED="${BUILD//\//\\/}"
+
+sed -i "s/^.*app.build:.*/    app.build: ${BUILD_ESCAPED}/" $PROJECT_DIR/config/services.yaml
